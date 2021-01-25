847e5581b00ae60881c7ed0387560029f8d33a1a
Peter-The-Great <PjotrW15@Gmail.com>
2021-01-25 11:34:57 +0100

    Beginning of the end

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <?php require("components/style.php");?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+
+</head>
+<body>
+<?php require("components/scripts.php");?>
+</body>
+</html>
