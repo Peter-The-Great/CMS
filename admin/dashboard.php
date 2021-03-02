35af500f29695ffab0d8441af7709a3b7a4eb296
Peter-The-Great <PjotrW15@Gmail.com>
2021-03-02 10:01:12 +0100

    Laatste

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -38,7 +38,24 @@ $id = $_SESSION['id'];
 
 				<?php
 				foreach ($result as $item) {
-					echo "<td>" . $item["title"] . "</td><td>" .  $item["date"] . "</td></td><td><a href='../post.php?id=" . $item['id'] . "' class='btn btn-info btn-lg'><i class='fas fa-eye'></i></a></td><td><a href='changepost.php?id=" . $item['id'] . "' class='btn btn-warning btn-lg'><i class='fas fa-user-edit'></i></a></td><td><a href='../php/removepost.php?id=" . $item["id"] . "' class='btn btn-danger btn-lg'><i class='fas fa-trash-alt'></i></a></td></tr>";
+					echo "<td>" . $item["title"] . "</td><td>" .  $item["date"] . "</td></td><td><a href='../post.php?id=" . $item['id'] . "' class='btn btn-info btn-lg'><i class='fas fa-eye'></i></a></td><td><a href='changepost.php?id=" . $item['id'] . "' class='btn btn-warning btn-lg'><i class='fas fa-user-edit'></i></a></td><td><button type='button' data-bs-toggle='modal' data-bs-target='#post". $item['id'] ."' class='btn btn-danger btn-lg'><i class='fas fa-trash-alt'></i></button></td></tr>
+					<div class='modal fade' id='post". $item['id'] ."' data-bs-backdrop='static' data-bs-keyboard='false' tabindex='-1' aria-labelledby='postlabel". $item['id'] ."' aria-hidden='true'>
+                        <div class='modal-dialog'>
+                          <div class='modal-content'>
+                            <div class='modal-header'>
+                              <h5 class='modal-title' id='postlabel". $item['id'] ."'>Modal title</h5>
+                              <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                            </div>
+                            <div class='modal-body'>
+                            <b>Weet je zeker dat je de post wilt verwijderen? Deze actie kan niet ongedaan worden!</b>
+                            </div>
+                            <div class='modal-footer'>
+                              <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
+                              <a href='../php/removepost.php?id=" . $item["id"] . "'><button type='button' class='btn btn-danger'>Understood</button></a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>";
 				}
 				?>
 			</tbody>
